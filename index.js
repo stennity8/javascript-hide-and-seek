47668d82bd77ee924ba94296465928d2d6e21b82
stennity8 <42423580+stennity8@users.noreply.github.com>
2019-03-04 18:44:33 -0500

    Added solutions

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
+function getFirstSelector(s) {
+  return document.querySelector(s);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
+
+//My Solution
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n){
+  const list = document.querySelectorAll('li');
+
+  for (let i = 0; i < list.length; i++){
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
+
+function deepestChild(){
+  const children = document.querySelectorAll('#grand-node div');
+  return children[children.length - 1];
+}
